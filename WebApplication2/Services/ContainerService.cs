c27fa1fe2af3b73e1ee8d3f3adf88625f562985d
Hanish Maram <hanish.maram@ert.com>
2021-12-14 20:12:51 +0530

    Add project files.

---
+++ WebApplication2/Services/ContainerService.cs
@@ -0,0 +1,34 @@
+﻿using Azure.Storage.Blobs;
+using WebApplication2.Services;
+
+namespace AzureBlobProject.Services
+{
+    public class ContainerService : IContainerService
+    {
+        private readonly BlobServiceClient _blobClient;
+
+        public ContainerService(BlobServiceClient blobClient)
+        {
+            _blobClient = blobClient;
+        }
+        public Task CreateContainer(string containerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteContainer(string containerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<string>> GetAllContainers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<string>> GetAllContainersAndBlobs()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebApplication2/Services/IContainerService.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication2.Services
+{
+    public interface IContainerService
+    {
+        Task<List<string>> GetAllContainersAndBlobs();
+        Task<List<string>> GetAllContainers();
+        Task CreateContainer(string containerName);
+        Task DeleteContainer(string containerName);
+    }
+}
